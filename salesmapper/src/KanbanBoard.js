048fe6b32aa8b58904d83d8bf4b7d5fde8ae9cac
stefanverleysen <stef@asan.digital>
2023-12-02 17:24:25 -0500

    GO GO

--- salesmapper/src/KanbanBoard.js
+++ salesmapper/src/KanbanBoard.js
@@ -2,32 +2,44 @@ import React from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const KanbanBoard = ({ stages, activities, milestones, onDragEnd }) => {
-    return (
-        <DragDropContext onDragEnd={onDragEnd}>
-            {stages.map((stage, stageIndex) => (
-                <Droppable droppableId={`stage-${stage.id}`} key={stage.id}>
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="kanban-board">
+        {stages.map((stage, stageIndex) => (
+          <Droppable droppableId={`stage-${stage.id}`} key={stage.id}>
+            {(provided) => (
+              <div
+                className="kanban-column"
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                <h3>{stage.name}</h3>
+                {activities.map((activity, index) => (
+                  <Draggable
+                    key={activity.id}
+                    draggableId={`activity-${activity.id}`}
+                    index={index}
+                  >
                     {(provided) => (
-                        <div ref={provided.innerRef} {...provided.droppableProps}>
-                            <h3>{stage.name}</h3>
-                            {activities.map((activity, index) => (
-                                <Draggable key={activity.id} draggableId={`activity-${activity.id}`} index={index}>
-                                    {(provided) => (
-                                        <div
-                                            ref={provided.innerRef}
-                                            {...provided.draggableProps}
-                                            {...provided.dragHandleProps}>
-                                            {activity.name}
-                                        </div>
-                                    )}
-                                </Draggable>
-                            ))}
-                            {provided.placeholder}
-                        </div>
+                      <div
+                        className="kanban-card"
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        {activity.name}
+                      </div>
                     )}
-                </Droppable>
-            ))}
-        </DragDropContext>
-    );
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        ))}
+      </div>
+    </DragDropContext>
+  );
 };
 
 export default KanbanBoard;
